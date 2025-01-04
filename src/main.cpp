4d22899348285f06d4ccfaeca13dde9a040a870a
Elijah Stephenson <elijah.m.stephenson@gmail.com>
2025-01-03 21:39:01 -0600

    Fixed axis rotation for real this time

--- src/main.cpp
+++ src/main.cpp
@@ -148,9 +148,9 @@ void updateAngles() {
             logString(euler.angle.yaw, true);
         }
     } else {
-        hat.gyro[0] = -euler.angle.yaw;
-        hat.gyro[1] = -euler.angle.pitch;
-        hat.gyro[2] = euler.angle.roll;
+        hat.gyro[0] = -euler.angle.yaw; //Yaw
+        hat.gyro[1] = euler.angle.pitch; //Actually Roll
+        hat.gyro[2] = -euler.angle.roll; //Actually Pitch
         sendAnglesToHatire();
     }
 }
